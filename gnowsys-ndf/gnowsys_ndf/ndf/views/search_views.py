5183503242a67d9d39ccac02cca0550a63b46a1f
siddhu dhangar <siddhudhangar@gmail.com>
2018-08-05 21:12:14 +0530

    In search_views.py file, added if else condition for metastudio.so that resource will be searched without checking its educationaluse

--- gnowsys-ndf/gnowsys_ndf/ndf/views/search_views.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/search_views.py
@@ -29,7 +29,7 @@ from django.shortcuts import render_to_response  # , render
 from elasticsearch_dsl.query import MultiMatch, Match
 from gnowsys_ndf.ndf.gstudio_es.es import *
 from gnowsys_ndf.ndf.gstudio_es.paginator import Paginator ,EmptyPage, PageNotAnInteger
-
+from gnowsys_ndf.settings import GSTUDIO_SITE_NAME
 
 my_doc_requirement = u'storing_orignal_doc'
 reduced_doc_requirement = u'storing_reduced_doc'
@@ -227,8 +227,11 @@ def results_search(request, group_id, page_no=1, return_only_dict = None):
 			# 	for value in fields:
 			# 		value=value+"=on&"
 			# 		strconcat = strconcat + value
-
-			q = Q('bool', must=[Q('multi_match', query=search_text, fields=['content','name','tags','content_org','altnames']),Q('terms',attribute_set__educationaluse=['documents','images','audios','videos','interactives','ebooks']),Q('match', group_set=str(group_id))],
+			if GSTUDIO_SITE_NAME == "NROER":
+				q = Q('bool', must=[Q('multi_match', query=search_text, fields=['content','name','tags','content_org','altnames']),Q('terms',attribute_set__educationaluse=['documents','images','audios','videos','interactives','ebooks']),Q('match', group_set=str(group_id))],
+                          should=[Q('match',member_of=GST_FILE1.hits[0].id),Q('match',member_of=GST_IPAGE1.hits[0].id),Q('match',member_of=GST_JSMOL1.hits[0].id),Q('match',member_of=GST_PAGE1.hits[0].id)],minimum_should_match=1)
+			else:
+				q = Q('bool', must=[Q('multi_match', query=search_text, fields=['content','name','tags','content_org','altnames']),Q('match', group_set=str(group_id))],
                           should=[Q('match',member_of=GST_FILE1.hits[0].id),Q('match',member_of=GST_IPAGE1.hits[0].id),Q('match',member_of=GST_JSMOL1.hits[0].id),Q('match',member_of=GST_PAGE1.hits[0].id)],minimum_should_match=1)
 
 			search_result =Search(using=es, index="nodes",doc_type="gsystemtype,gsystem,metatype,relationtype,attribute_type,group,author").query(q)
