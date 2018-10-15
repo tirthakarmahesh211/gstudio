01837ded28e43d4e60d8bae140d88c20be8e4b97
siddhu dhangar <siddhudhangar@gmail.com>
2018-10-15 14:38:31 +0530

    In search results, workspace cards are shown in search page

--- gnowsys-ndf/gnowsys_ndf/ndf/views/search_views.py
+++ gnowsys-ndf/gnowsys_ndf/ndf/views/search_views.py
@@ -249,12 +249,11 @@ def results_search(request, group_id, page_no=1, return_only_dict = None,ws = Fa
 		elif request.GET.get('search_text',None) and group_id_str == "ws":
 
 			if GSTUDIO_SITE_NAME == "NROER":
-				print "search nroer"
 				q = Q('bool', must=[Q('multi_match', query=search_text, fields=['content','name^4','tags^3','altnames^5']),Q('terms',attribute_set__educationaluse=['documents','images','audios','videos','interactives','ebooks']),Q('match', access_policy='public')],
                           should=[Q('match',member_of=GST_FILE1.hits[0].id),Q('match',member_of=GST_IPAGE1.hits[0].id),Q('match',member_of=GST_JSMOL1.hits[0].id),Q('match',member_of=GST_PAGE1.hits[0].id)],minimum_should_match=1)
 			else:
 				q = Q('bool', must=[Q('multi_match', query=search_text, fields=['content','name^4','tags^3','altnames^5']),~Q('match', group_set=str(trash_groupid))],
-                          should=[Q('match',member_of=GST_FILE1.hits[0].id),Q('match',member_of=GST_IPAGE1.hits[0].id),Q('match',member_of=GST_JSMOL1.hits[0].id),Q('match',member_of=GST_PAGE1.hits[0].id)],minimum_should_match=1)
+                          should=[Q('match',member_of=GST_GROUP1.hits[0].id),Q('match',member_of=GST_FILE1.hits[0].id),Q('match',member_of=GST_IPAGE1.hits[0].id),Q('match',member_of=GST_JSMOL1.hits[0].id),Q('match',member_of=GST_PAGE1.hits[0].id)],minimum_should_match=1)
 
 			search_result =Search(using=es, index=index,doc_type="gsystemtype,gsystem,metatype,relationtype,attribute_type,group,author").query(q)
 
